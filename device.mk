a0d7fc150c88db0d5c90e9c8afb0e1d762597653
myfluxi <linflux@arcor.de>
2014-09-02 16:20:14 +0200

    hammerhead: Update fluence settings to match HAL
    Change-Id: I72c4cdc0f9b7b42f998ebf54533e23d8c36cdb3f

--- device.mk
+++ device.mk
@@ -292,11 +292,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Audio Configuration
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.audio.handset.mic.type=digital \
-    persist.audio.dualmic.config=endfire \
+    ro.qc.sdk.audio.fluencetype=fluence \
     persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=false \
     af.resampler.quality=4 \
     audio.offload.buffer.size.kb=32 \
     audio.offload.gapless.enabled=false \
